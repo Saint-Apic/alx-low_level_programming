39790add530a91919a2dead24f9d69e0e5ceb77d
Chukwuka-Isaac Saint-Apic Peter <saintpeterchukwuka@gmail.com>
2022-09-23 04:49:22 -0700

    a C program that prints exactly Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+vim 4-puts.c
+#include <stdio.h>
+/**
+ * main-Printing c code
+ *
+ * Return: Always 0 (Successful)
+ */
+int main (void)
+{
+	puts("\ "Programming is like building a multilingual puzzle");
+	return(0);
+}
