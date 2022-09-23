66be12c8559fbf53f55961bc7ea7b404b218ca1d
Chukwuka-Isaac Saint-Apic Peter <saintpeterchukwuka@gmail.com>
2022-09-23 06:48:34 -0700

    new edit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
+
 /**
- * main-Printing c code
+ *Description: main-Printing c code
  *
  * Return: Always 0 (Successful)
  */
 int main (void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
